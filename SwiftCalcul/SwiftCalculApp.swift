de705151f46eed5176082c6fa5deb3e2375314af
Tristepin222 <Tristepin22@gmail.com>
2024-03-15 11:43:11 +0100

    Initial Commit

---
+++ SwiftCalcul/SwiftCalculApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCalculApp.swift
+//  SwiftCalcul
+//
+//  Created by tristepin222 on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalculApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
